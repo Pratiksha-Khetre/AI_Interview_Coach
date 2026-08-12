703744b80a8d537246f8b9072372798cdb621042
Pratiksha-khetre <pratikshakhetre1011@gmail.com>
2026-08-12 23:50:59 +0530

    Fix Render Python runtime and Groq dependency

--- Backend/app/services/vector_services.py
+++ Backend/app/services/vector_services.py
@@ -1,21 +1,35 @@
-# Backend\app\services\vector_services.py
-
 from langchain_community.vectorstores import FAISS
 from langchain_core.documents import Document
-from pathlib import Path
 from langchain_huggingface import HuggingFaceEmbeddings
+from pathlib import Path
 
-EMBEDDING_MODEL = HuggingFaceEmbeddings(
-    model_name="sentence-transformers/all-MiniLM-L6-v2"
-)
 
-def create_vector_store(documents : list[Document]):
-    vector_store = FAISS.from_documents(documents, EMBEDDDING_MODEL)       #FAISS stores associated documets + embeddings
+_embedding_model = None
+
+
+def get_embedding_model():
+    global _embedding_model
+
+    if _embedding_model is None:
+        _embedding_model = HuggingFaceEmbeddings(
+            model_name="sentence-transformers/all-MiniLM-L6-v2"
+        )
+
+    return _embedding_model
+
+
+def create_vector_store(documents: list[Document]):
+    embedding_model = get_embedding_model()
+
+    vector_store = FAISS.from_documents(
+        documents,
+        embedding_model
+    )
 
     return vector_store
 
 
-def save_vector_store(vector_store : FAISS, resume_id : str) -> None:
+def save_vector_store(vector_store: FAISS, resume_id: str) -> None:
 
     store_dir = Path("vector_store") / resume_id
     store_dir.mkdir(parents=True, exist_ok=True)
@@ -23,11 +37,16 @@ def save_vector_store(vector_store : FAISS, resume_id : str) -> None:
     vector_store.save_local(store_dir)
 
 
-def load_vector_store(resume_id : str) -> FAISS:
+def load_vector_store(resume_id: str) -> FAISS:
 
     store_dir = Path("vector_store") / resume_id
 
-    vector_store = FAISS.load_local(store_dir, EMBEDDDING_MODEL, allow_dangerous_deserialization= True)
+    embedding_model = get_embedding_model()
+
+    vector_store = FAISS.load_local(
+        store_dir,
+        embedding_model,
+        allow_dangerous_deserialization=True
+    )
 
     return vector_store
-
