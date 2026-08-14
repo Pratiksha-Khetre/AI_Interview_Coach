5fbaa6e1f1e76d0d57564777c77d60a89e3b2c87
Pratiksha-khetre <pratikshakhetre1011@gmail.com>
2026-08-14 13:33:40 +0530

    add health point

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -4,17 +4,25 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.api.routes.interview import router as interview_router
 from app.api.routes.resume import router as resume_router
 
+
 app = FastAPI()
+
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
+
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000",
+        "http://localhost:3000"
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 app.include_router(interview_router)
 app.include_router(resume_router)
