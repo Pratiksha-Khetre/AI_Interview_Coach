cb70ba12bd0d6d3c64efd5cfeab32d0ab4b2a662
Pratiksha-khetre <pratikshakhetre1011@gmail.com>
2026-08-17 22:45:26 +0530

    add health point

--- Backend/app/services/rag_service.py
+++ Backend/app/services/rag_service.py
@@ -1,24 +1,39 @@
+# Backend\app\services\rag_service.py
+
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_core.documents import Document
 
-def chunk_text(text: str, chunk_size=500, chunk_overlap=50) -> list[str]:
+def chunk_text(text : str, chunk_size = 500, chunk_overlap = 50) -> list[str]:
     text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=chunk_size,
-        chunk_overlap=chunk_overlap,
-        length_function=len,
-        separators=["\n\n", "\n", " ", ""]
+        chunk_size = chunk_size,
+        chunk_overlap = chunk_overlap,
+        length_function = len,
+        separators = ["\n\n", "\n", " ", ""]
     )
 
-    return text_splitter.split_text(text)
+    chunks = text_splitter.split_text(text)
+
+    return chunks
+
+# from sentence_transformers import SentenceTransformer
+# model = SentenceTransformer("all-MiniLM-L6-v2")
+
+# def create_embeddings(chunks : list[str]):
+
+#     embeddings = model.encode(chunks)
+
+#     return embeddings
 
 
-def create_document(chunks: list[str], metadata: dict) -> list[Document]:
+
+def create_document(chunks : list[str], metadata : dict) -> list[Document]:
     documents = []
-
+    
     for chunk in chunks:
+
         document = Document(
-            page_content=chunk,
-            metadata=metadata
+            page_content= chunk,
+            metadata = metadata
         )
 
         documents.append(document)
--- Backend/app/services/retrieval_service.py
+++ Backend/app/services/retrieval_service.py
@@ -1,12 +1,22 @@
-# Backend\app\services\retrieval_service.py
-
-from langchain_core.documents import Document
 from app.services import vector_services
 
-def retrieve_context(resume_id : str, query : str, k : int = 3) -> list[Document]:
 
+def retrieve_context(
+    resume_id: str,
+    query: str,
+    k: int = 5
+):
     vector_store = vector_services.load_vector_store(resume_id)
 
-    documents = vector_store.similarity_search(query, k = k)
+    results = vector_services.search_vector_store(
+        vector_store,
+        query,
+        k=k
+    )
+
+    documents = [
+        result["document"]
+        for result in results
+    ]
 
     return documents
